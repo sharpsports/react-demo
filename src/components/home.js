d7d2acbf458abafdd1a06b8692356e18ff36ad7d
Samuel Coolidge <samuelcoolidge@Samuels-MacBook-Pro.local>
2020-10-06 16:59:41 -0700

    initial sandbox demo commit

--- src/components/home.js
+++ src/components/home.js
@@ -2,18 +2,36 @@ import React from 'react';
 
 
 class Home extends React.Component {
-  
+
+    constructor(props) {
+      super(props);
+      this.state = { width: 0, height: 0 };
+      this.updateWindowDimensions = this.updateWindowDimensions.bind(this);
+    }
+
+    componentDidMount() {
+      this.updateWindowDimensions();
+      window.addEventListener('resize', this.updateWindowDimensions);
+    }
+
+    componentWillUnmount() {
+      window.removeEventListener('resize', this.updateWindowDimensions);
+    }
+
+    updateWindowDimensions() {
+      this.setState({ width: window.innerWidth, height: window.innerHeight });
+    }
+
     render() {
       return (
         <div>
-        <div id="SSLink" style={{marginLeft:910, marginTop:172}}>
+        <div id="SSLink" style={{marginLeft:this.state.width*.65, marginTop:this.state.height*.215}}>
           <div></div>
         </div>
         </div>
-  
+
       );
     }
   }
-  
+
   export default Home;
-  
