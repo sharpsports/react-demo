ff0d466d9a951f88cb1f523fd0faaeca566cbb37
Ryan Murphy <rpmurph4@gmail.com>
2020-10-19 18:58:23 -0700

    halp

--- src/App.js
+++ src/App.js
@@ -1,30 +1,36 @@
-import React, { Component } from 'react';
+import React, { Component,useState } from 'react';
 import './App.css';
 import Home from './components/home';
 import Webhook from './components/webhook';
 import { BrowserRouter, Route, Switch, Link } from 'react-router-dom';
+import Header from './components/Header/header';
+import RegistrationForm from './components/RegistrationForm/RegistrationForm';
+import LoginForm from './components/LoginForm/LoginForm';
 
-class App extends React.Component {
+function App() {
 
-  constructor(props) {
-    super(props);
-  }
-
-  render() {
+  const [title, updateTitle] = useState(null);
+  const [errorMessage, updateErrorMessage] = useState(null);
 
       return (
-
         <BrowserRouter>
              <Switch>
-              <Route path="/" component={Home}>
-              </Route>
-              <Route path="/webhook" component={Webhook}>
-              </Route>
-            </Switch>
+                <Route path="/" exact={true}>
+                  <RegistrationForm showError={updateErrorMessage} updateTitle={updateTitle}/>
+                </Route>
+                <Route path="/register">
+                  <RegistrationForm showError={updateErrorMessage} updateTitle={updateTitle}/>
+                </Route>
+                <Route path="/login">
+                  <LoginForm showError={updateErrorMessage} updateTitle={updateTitle}/>
+                </Route>   
+                <Route path="/home">
+                  <Home showError={updateErrorMessage} updateTitle={updateTitle}/>
+                </Route>         
+              </Switch>
        </BrowserRouter>
-
       );
-    }
+    
   }
 
 export default App;
--- src/components/home.js
+++ src/components/home.js
@@ -1,37 +1,102 @@
-import React from 'react';
+import React,{ useEffect } from 'react';
+import { withRouter } from 'react-router-dom';
+import { Helmet } from 'react-helmet';
+// import { ACCESS_TOKEN_NAME, API_BASE_URL } from '../../constants/apiConstants';
+import axios from 'axios';
+import Table from './betTable';
 
 
-class Home extends React.Component {
+function Home(props) {
+    // var email;
+    useEffect(() => {
+        axios.get('http://localhost:4000/user/me', { headers: { 'token': localStorage.getItem('token') }})
+        .then(function (response) {
+            if(response.status !== 200){
+              redirectToLogin()
+            }
+            console.log(response.data);
+            localStorage.setItem('email',response.data.email);
+            let bettorid = response.data.bettorId;
+            
+            axios.post(`https://api.sharpsports.io/v1/bettors/${bettorid}/refreshbets`,{},{headers: { 'Authorization': 'Token 1fb886d9aff543cb6e2d87691a8b977abf12d312' }});
+            console.log(bettorid);
 
-    constructor(props) {
-      super(props);
-      this.state = { width: 0, height: 0 };
-      this.updateWindowDimensions = this.updateWindowDimensions.bind(this);
+            // axios.get('http://localhost:4000/user/bets', {headers: { 'token': localStorage.getItem('token') }})
+            // .then(function (response) {
+            //   // var bets = response.data;
+            //   var bets = response.data;
+            //   var betsArray = []
+            //   var i;
+            //   for (i=0;i<bets.length;i++) {
+            //     // console.log(bets[i]);
+            //     betsArray.push(bets[i])
+            //   }
+            //   console.log(betsArray)
+            //   localStorage.setItem('bets',JSON.stringify(betsArray));
+            // })
+        })
+        .catch(function (error) {
+          redirectToLogin()
+        });
+
+      })
+    function redirectToLogin() {
+    props.history.push('/login');
     }
 
-    componentDidMount() {
-      this.updateWindowDimensions();
-      window.addEventListener('resize', this.updateWindowDimensions);
-    }
+    const betsArray = [];
+    
+    const all_bets = axios.get('http://localhost:4000/user/bets', {headers: { 'token': localStorage.getItem('token') }})
+    .then(function (response) {
+      var bets = response.data;
+      console.log(bets);
+      // bets = response.data;
+      var i;
+      for (i=0;i<bets.length;i++) {
+        // console.log(bets[i]);
+        betsArray.push(bets[i])
+      }
+    });
 
-    componentWillUnmount() {
-      window.removeEventListener('resize', this.updateWindowDimensions);
-    }
+    console.log(all_bets);
+    const email = localStorage.getItem('email');
+    console.log(email);
 
-    updateWindowDimensions() {
-      this.setState({ width: window.innerWidth, height: window.innerHeight });
-    }
+    // const mappedbets = all_bets.map((d) => {return <li key={d.contest}>{d.contest}</li>});
 
-    render() {
-      return (
+    console.log(Object.keys(all_bets));
+
+    
+    return (
+      <div>
+        <Helmet>
+          <script src="https://d388bvybj12fcd.cloudfront.net/button.js"
+            token = "1fb886d9aff543cb6e2d87691a8b977abf12d312"
+            internalid = {String(email)}
+
+            buttonText='BookLink'
+            padding='15px 32px'
+            background-color='#2b2c2e'
+            border='none'
+            color='#2ce384'
+            text-align='center'
+            text-decoration='none'
+            display='inline-block'
+            font-size='20px'
+            border-radius='8px'
+            font-family='Impact,Charcoal,sans-serif'
+            logo-url='https://sample-app-logo.s3.amazonaws.com/sample-logo-final-small.png'
+            defer>
+          </script>
+        </Helmet>
+        <div id="SSLink" style={{top:"5%", left:"5%",position:"absolute"}}>
+        </div>
         <div>
-        <div id="SSLink" style={{marginLeft:this.state.width*.65, marginTop:this.state.height*.215}}>
-          <div></div>
+          {/* {mappedbets} */}
         </div>
-        </div>
+      </div>
 
-      );
-    }
-  }
+    );
+}
 
-  export default Home;
+export default withRouter(Home);
