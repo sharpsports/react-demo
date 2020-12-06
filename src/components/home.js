50107ee437104185b5919d50c5a1effecbf15f4d
Samuel Coolidge <samuelcoolidge@Samuels-MacBook-Pro.local>
2020-12-05 19:42:35 -0500

    added email check

--- src/components/home.js
+++ src/components/home.js
@@ -65,10 +65,12 @@ class Home extends React.Component {
 
     var keys = null;
     var header = null;
+    /*
     if (this.state.bets){
       keys = Object.keys(this.state.bets[0]);
       header = keys.map((key, index) => {return <th key={key} scope="col">{key.toUpperCase()}</th>});
     }
+    */
 
     var betdata = null;
     if (this.state.bets){
@@ -90,54 +92,62 @@ class Home extends React.Component {
     //   mappedbets = this.state.bets;
     // }
 
+    var email = this.state.email
+    console.log(email);
 
 
-    return (
-        <div>
-        <ScriptTag type="text/javascript" src="https://d388bvybj12fcd.cloudfront.net/button.js"
-            token = "1fb886d9aff543cb6e2d87691a8b977abf12d312"
-            internalid = {String(this.state.email)}
-
-            buttonText='BookLink'
-            padding='15px 32px'
-            background-color='#2b2c2e'
-            border='none'
-            color='#2ce384'
-            text-align='center'
-            text-decoration='none'
-            // display='inline-block'
-            font-size='20px'
-            border-radius='8px'
-            font-family='Impact,Charcoal,sans-serif'
-            logo-url='https://sample-app-logo.s3.amazonaws.com/sample-logo-final-small.png'/>
-          <div id="SSLink" style={{top:"5%", left:"5%",position:"absolute"}}>
-          </div>
-          <br></br>
-          <br></br>
-          <br></br>
-          <br></br>
-          <br></br>
+    if (email) {
+      return (
           <div>
-            <table class="table table-striped table-dark">
-              <thead>
-                <tr>{header}</tr>
-              </thead>
-              <tbody>
-                {betdata}
-              </tbody>
-            </table>
+          <ScriptTag type="text/javascript" src="https://d388bvybj12fcd.cloudfront.net/button.js"
+              token = "1fb886d9aff543cb6e2d87691a8b977abf12d312"
+              internalid = {email}
+              buttonText='BookLink'
+              padding='15px 32px'
+              background-color='#2b2c2e'
+              border='none'
+              color='#2ce384'
+              text-align='center'
+              text-decoration='none'
+              // display='inline-block'
+              font-size='20px'
+              border-radius='8px'
+              font-family='Impact,Charcoal,sans-serif'
+              logo-url='https://sample-app-logo.s3.amazonaws.com/sample-logo-final-small.png'/>
+            <div id="SSLink" style={{top:"5%", left:"5%",position:"absolute"}}>
+            </div>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            <div>
+              <table class="table table-striped table-dark">
+                <thead>
+                  <tr>{header}</tr>
+                </thead>
+                <tbody>
+                  {betdata}
+                </tbody>
+              </table>
+            </div>
+            {/* <FilterableTable
+                namespace="Bets"
+                initialSort="contest"
+                data={mappedbets}
+                fields={fields}
+                noRecordsMessage="There are no people to display"
+                noFilteredRecordsMessage="No people match your filters!"
+              /> */}
           </div>
-          {/* <FilterableTable
-              namespace="Bets"
-              initialSort="contest"
-              data={mappedbets}
-              fields={fields}
-              noRecordsMessage="There are no people to display"
-              noFilteredRecordsMessage="No people match your filters!"
-            /> */}
+      );
+    }
+    else {
+      return (
+        <div>
         </div>
-    );
-
+      );
+    }
 }
 
 }
