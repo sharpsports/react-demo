91d6813a866bfa2220b4aba65e7ea1ad0967c6ed
Samuel Coolidge <sam.r.coolidge@gmail.com>
2023-06-27 18:55:36 -0400

    added env vars + README

--- README.md
+++ README.md
@@ -1,31 +1,54 @@
 # Demo React app for SharpSports Integration
 
+
+
 ## Getting started
 
+ 
 ### Installation `npm install`
 
+
 This will install all necessary node dependencies. You need to have node and npm installed on your machine.
-
-### Variables
-
-Set the following variables in public/index.html
-
-- token: set this to your sandbox API key
-- internalId: set this to a test internal id value
-
-You can also change the styling of the button using the other parameters in SharpSports button script tag in the index.html file.
-
-### Button Implementation
-
-You can see the implementation of the button in src/components/home.js
 
 ### Run `npm start`
 
 You can now view the demo at http://localhost:3000
 
-### Adding different Bettors and BettorAccounts
 
-- Every time you change the "internalId" value in the button code in index.html this will indicate to the SharpSports API that a different end user (Bettor)
-is interacting with the button.
-- Once a new BettorAccount is linked by clicking the "Link SportsBook" button and entering valid test user credentials (http://docs.sharpsports.io/#sandbox-and-test-users) a BettorAccount and Bettor object is created in our database.
-- You can then interact with the Bettors/BettorAccounts as well as randomly generated test Bets using the endpoints found at http://docs.sharpsports.io/#core-resources.
+## Environment Variables
+
+  
+Update a `.env` file in the root directory to set environment variables. You must re-run `npm start` in order to reload env vars
+
+**Required Variables**
+
+- REACT_APP_API_BASE: API base url for connection with SharpSports API (e.g. https://api.sharpsports.io, http://localhost:8000)
+- REACT_APP_UI_BASE: UI base url for frontend enviornment (e.g. https://ui.sharpsports.io, http://localhost:3006)
+- REACT_APP_INTERNAL_ID: InternalID of test bettor
+- REACT_APP_PRIVATE_KEY: Test user private key
+	- Live: `433b0432d117a4c9ae338bd2e8467175d67af829`
+	- Sandbox: `1fb886d9aff543cb6e2d87691a8b977abf12d312`
+- REACT_APP_PUBLIC KEY: Test user public key
+	- Live: `a4e27d45042947e7967146c26973bbd4a4e27d45`
+	- Sandbox: `1fb886d9aff543cb6e2d87691a8b977abf12d312`
+
+ **Optional Testing Variables**
+
+- REACT_APP_SERVICE: Context service query param, will also add add required auth
+	- React Native: sharpsports-mobile-npm_{version}
+	- Android Native: sharpsports-mobile-android_{version}
+	- iOS Native: sharpsports-mobile-spm_{version}
+	- Cordova: sharpsports-mobile-cordova_{version}
+- REACT_APP_EXTENSION_BOOL: True if want extensionAuthToken on Context
+- REACT_APP_USERAGENT: Spoofed UserAgent for testing
+	- Android Example: Mozilla/5.0 (Linux; Android 13) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.5735.130 Mobile Safari/537.36
+	- iOS Example: Mozilla/5.0 (iPhone; CPU iPhone OS 16_5_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/16.5 Mobile/15E148 Safari/604.1
+	- Chrome Example: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36
+
+
+ ## Manual Setup
+
+- If you change the publicKey, privateKey, or internalID variables in `.env` file you will need to update the script tag values in `index.html` to match, including computed extensionAuthToken (same as auth value in `home.js`
+- If you wish to test without extension implementation then comment out `https://d1vhnbpkpweicq.cloudfront.net/extension-cdn.js` script tag altogether
+- Download/Delete SharpSports Chrome Extension and start a new browser tab for testing this path
+
--- src/components/home.js
+++ src/components/home.js
@@ -10,10 +10,14 @@ class Home extends React.Component {
       super(props);
       this.state = { width: 0, height: 0 };
       this.updateWindowDimensions = this.updateWindowDimensions.bind(this);
-      this.internalId = 'extension-test'
-      this.publicKey = 'a4e27d45042947e7967146c26973bbd4a4e27d45'
-      this.privateKey = '433b0432d117a4c9ae338bd2e8467175d67af829'
-      this.extensionAuthToken = md5(this.internalId + this.privateKey)
+      this.internalId = process.env.REACT_APP_INTERNAL_ID
+      this.privateKey = process.env.REACT_APP_PRIVATE_KEY
+      this.publicKey = process.env.REACT_APP_PUBLIC_KEY
+      this.extension = (process.env.REACT_APP_EXTENSION_BOOL === 'true')
+      this.service = process.env.REACT_APP_SERVICE
+      this.userAgent = process.env.REACT_APP_USERAGENT
+      this.extensionVersion = sessionStorage.getItem("sharpSportsExtensionVersion");
+      this.auth = md5(this.internalId + this.privateKey)
     }
 
     componentDidMount() {
@@ -27,7 +31,7 @@ class Home extends React.Component {
       this.setState({ width: window.innerWidth, height: window.innerHeight });
     }
 
-    async postContext(url, data) {
+    async postContext(url, body) {
 
       const response = await fetch(url, {
         method: 'POST',
@@ -35,7 +39,7 @@ class Home extends React.Component {
           'Authorization': `Token ${this.publicKey}`,
           'Content-Type': 'application/json'
         },
-        body: JSON.stringify(data)
+        body: JSON.stringify(body)
       });
       return response.json();
     }
@@ -45,28 +49,54 @@ class Home extends React.Component {
       const y = win.top.outerHeight / 2 + win.top.screenY - ( h / 2);
       const x = win.top.outerWidth / 2 + win.top.screenX - ( w / 2);
       return win.open(url, title, `toolbar=no, location=no, directories=no, status=no, menubar=no, scrollbars=no, resizable=no, copyhistory=no, width=${w}, height=${h}, top=${y}, left=${x}`);
-  }
+
+    }
   
     
     onLinkPopUp() {
-      this.postContext('https://api.sharpsports.io/v1/context',{internalId:this.internalId, extensionAuthToken: this.extensionAuthToken})
+      console.log("Auth Token: ", this.auth)
+      let body = {
+        internalId: this.internalId,
+        extensionAuthToken: (this.extension) ? this.auth : null,
+        bettorToken: (this.service) ? this.auth : null
+      }
+      let url = `${process.env.REACT_APP_API_BASE}/v1/context`
+      if (this.service) url = url.concat(`?service=${this.service}`)
+      this.postContext(url,body)
       .then(data => {
         console.log(data)
-        this.popupWindow(`https://ui.sharpsports.io/link/${data.cid}`,'SharpSports',window,500,600)
+        let url = `${process.env.REACT_APP_UI_BASE}/link/${data.cid}`;
+        if (this.userAgent) url = url.concat(`?userAgent=${this.userAgent}`);
+        window.location.href = url;
+        this.popupWindow(url,'SharpSports',window,500,600)
       })
     }
 
     onLinkRedirect() {
+      console.log("Auth Token: ", this.auth)
       let redirectUrl = window.location.href;
-      this.postContext('https://api.sharpsports.io/v1/context',{internalId:this.internalId, extensionAuthToken: this.extensionAuthToken, redirectUrl:redirectUrl})
+      let body = {
+        internalId: this.internalId,
+        extensionAuthToken: (this.extension) ? this.auth : null,
+        bettorToken: (this.service) ? this.auth : null,
+        redirectUrl
+      }
+      let url = `${process.env.REACT_APP_API_BASE}/v1/context`
+      if (this.service) url = url.concat(`?service=${this.service}`)
+      this.postContext(url,body)
       .then(data => {
-        window.location.href = `https://ui.sharpsports.io/link/${data.cid}`
+        console.log(data);
+        let url = `${process.env.REACT_APP_UI_BASE}/link/${data.cid}`;
+        if (this.userAgent) url = url.concat(`?userAgent=${this.userAgent}`);
+        window.location.href = url;
       })
     }
 
     async onRefresh() {
-      const sharpSportsExtensionVersion = sessionStorage.getItem("sharpSportsExtensionVersion")
-      const response = await fetch(`https://api.sharpsports.io/v1/bettors/${this.internalId}/refresh?auth=${this.extensionAuthToken}&extensionVersion=${sharpSportsExtensionVersion}`, {
+      const sharpSportsExtensionVersion = sessionStorage.getItem("sharpSportsExtensionVersion");
+      let url = `${process.env.REACT_APP_API_BASE}/v1/bettors/${this.internalId}/refresh`;
+      if (this.extension) url = url.concat(`?auth=${this.auth}&extensionVersion=${sharpSportsExtensionVersion}`)
+      const response = await fetch(url, {
         method: 'POST',
         headers: {
           'Authorization': `Token ${this.publicKey}`,
@@ -84,7 +114,11 @@ class Home extends React.Component {
 
       return (
         <div id="main">
-          <h1>My Extension Test</h1>
+          <h1>Demo Test Application</h1>
+          <p>Service: {this.service}</p>
+          <p>Extension Version: {this.extensionVersion ? this.extensionVersion : "Extension Script not Initialized"}</p>
+          <p>Extension Auth Passed: {this.extension.toString()}</p>
+          <p>UserAgent: {this.userAgent ? this.userAgent : 'Default Browser User Agent'}</p>
           <div><Button onClick={() => this.onLinkPopUp()}>Link Book with Popup</Button></div>
           <div><Button onClick={() => this.onLinkRedirect()}>Link Book with Page Redirect</Button></div>
           <div><Button onClick={() => this.onRefresh()}>Refresh</Button></div>
